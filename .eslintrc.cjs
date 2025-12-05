3222d8a63c22106dd4565b6ab9618751b1f5f7a8
Gabriel Gimenes <gabrielgrock@gmail.com>
2025-12-05 00:33:35 -0300

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:playwright/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "playwright/expect-expect": "off",
+  },
+};
